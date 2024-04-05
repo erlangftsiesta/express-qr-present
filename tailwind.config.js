bea7593f20bca5cafc5b026e99ad00ba4a547fe4
VannFullstackDev <vann.developer2@gmail.com>
2024-04-05 19:18:45 +0700

    change home ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        'hijau': {  DEFAULT: '#C80000',  50: '#FF8181',  100: '#FF6C6C',  200: '#FF4343',  300: '#FF1B1B',  400: '#F10000',  500: '#C80000',  600: '#900000',  700: '#580000',  800: '#200000',  900: '#000000',  950: '#000000'},
+        'usernameColor': '#0132BF',
       },
     },
   },
   variants: {},
   plugins: [require("daisyui")],
 };
+
